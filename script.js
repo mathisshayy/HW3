5a66d19de7ded67abe2f3afb395a2a406985a629
Shay Mathis <70081339+mathisshayy@users.noreply.github.com>
2020-10-31 11:45:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,23 @@
-//generate random password
 function generate(){
 
-    //set password length/complexity
     let complexity = document.getElementById("slider").value;
 
-    //possible password values
     let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
 
     let password = "";
 
-    //create for loop to choose password characters
     for(var i = 0; i <= complexity; i++){
         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
     }
 
-    //add password to textbox/display area
     document.getElementById("display").value = password;
 
-    //add password to previously generated password section
-    document.getElementById("lastNums").innerHTML += password + "<br>";
+    document.getElementById("PrevNumbers").innerHTML += password + "<br>";
 
 }
 
-//set default length display of 25
-document.getElementById("length").innerHTML = "Length: 25";
+document.getElementById("length").innerHTML = "Length: 10";
 
-//function to set length based on slider position
 document.getElementById("slider").oninput = function(){
 
     if(document.getElementById("slider").value > 0){
@@ -36,7 +28,6 @@ document.getElementById("slider").oninput = function(){
     }
 }
 
-//function to copy password to clipboard
 function copyPassword(){
 
     document.getElementById("display").select();
